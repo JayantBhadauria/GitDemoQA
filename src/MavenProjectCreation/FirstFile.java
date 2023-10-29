2ed6ca4ac4ba7fff2284d668f743505bc3ef30e2
Jayant Bhadauria <jayantbhadauria@gmail.com>
2023-10-29 13:34:07 +0530

    Merge 29Oct2023 4

--- src/MavenProjectCreation/FirstFile.java
+++ src/MavenProjectCreation/FirstFile.java
@@ -3,9 +3,10 @@ public class FirstFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Jayant Bhadauria");
-		System.out.println("Hello Shivam Bhadauria");
-		System.out.println("Hello Prabal Bhadauria");
+		
+		System.out.println("Testing Cloning ");
+		System.out.print("New Merge");
+		
 	}
 
 }
