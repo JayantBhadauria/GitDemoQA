a5c4099b865787beb3a80f5b259a6e2201395362
Jayant Bhadauria <jayantbhadauria@gmail.com>
2023-10-29 12:55:36 +0530

    New Branch merge

--- src/MavenProjectCreation/FirstFile.java
+++
@@ -1,9 +0,0 @@
-package MavenProjectCreation;
-public class FirstFile {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Jayant");
-	}
-
-}
